e8fbdd5f3e70e855120af90aa3db21e301b5bd3d
Stefan Nothaas <stefan.nothaas@hhu.de>
2018-07-13 15:20:01 +0200

    eval: Remove unused EvaluationTable

--- src/main/java/de/hhu/bsinfo/dxutils/Stopwatch.java
+++ src/main/java/de/hhu/bsinfo/dxutils/Stopwatch.java
@@ -14,7 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>
  */
 
-package de.hhu.bsinfo.dxutils.eval;
+package de.hhu.bsinfo.dxutils;
 
 import de.hhu.bsinfo.dxutils.unit.TimeUnit;
 
