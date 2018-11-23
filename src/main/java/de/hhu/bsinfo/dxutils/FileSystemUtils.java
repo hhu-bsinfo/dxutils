05b156163330d253952fe8e54f986119569a6aa5
Filip Krakowski <Filip.Krakowski@Uni-Duesseldorf.de>
2018-11-23 11:44:14 +0100

    Add CommandRunner class and utility method to detect NFS mounts

--- src/main/java/de/hhu/bsinfo/dxutils/FileSystemUtils.java
+++ src/main/java/de/hhu/bsinfo/dxutils/FileSystemUtils.java
@@ -1,5 +1,8 @@
 package de.hhu.bsinfo.dxutils;
 
+import de.hhu.bsinfo.dxutils.shell.CommandRunner;
+import org.jetbrains.annotations.NotNull;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -35,5 +38,20 @@ public final class FileSystemUtils {
                 .sorted(Comparator.reverseOrder())
                 .map(Path::toFile)
                 .forEach(File::delete);
+    }
+
+    /**
+     * Determines if the specified file lies within a network mount.
+     *
+     * @param p_file The file to check.
+     * @return True, if the file lies within a network mount; false else
+     */
+    public static boolean isNetworkMount(final @NotNull File p_file) {
+        try {
+            return CommandRunner.execute("stat -f -c %T " + p_file.getAbsolutePath()).equals("nfs");
+        } catch (IOException | InterruptedException p_e) {
+            p_e.printStackTrace();
+            return false;
+        }
     }
 }
